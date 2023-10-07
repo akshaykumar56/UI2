44317d24b175ff21ba23dd367912bb44883860a1
unknown <kshykumr@gmail.com>
2023-10-07 16:46:44 +0530

    v7.1

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -87,7 +87,7 @@ function Service() {
                 </div>
                 <div className='Project-text' style={{fontFamily: 'Montserrat'}}>
                 Streamlined lab operations at Indira Gandhi Medical College Shimla with a barcode-based LIMS for efficient sample tracking and online report access.
-                <Link to='/igmc' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
+                <Link onClick={()=>{window.scroll(0,0)}}to='/igmc' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
                 </div>
 </div>
   </div>;
@@ -99,7 +99,7 @@ case "2":   return <Carousel key={2} className='Web-Development' responsive={res
                 </div>
                 <div className='Project-text' style={{fontFamily: 'Montserrat'}}>
                 Empowered Himachal Pradesh farmers through Efruit Hub's WordPress site, enabling direct sales and income enhancement, backed by Razorpay and Shiprocket.
-                <Link to='/efruit' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
+                <Link onClick={()=>{window.scroll(0,0)}}to='/efruit' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
                 </div>
 </div>
 
@@ -109,7 +109,7 @@ case "2":   return <Carousel key={2} className='Web-Development' responsive={res
                 </div>
                 <div className='Project-text' style={{fontFamily: 'Montserrat'}}>
                 Built a dynamic HIMCO website using React and Strapi, combining robust functionality with seamless user experience.
-                <Link to='/himcofed' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
+                <Link onClick={()=>{window.scroll(0,0)}}to='/himcofed' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
                 </div>
 </div>
         <div className='Project-Box' style={{width:'300px',height:'450px',margin:'10px 50px',backgroundImage:`url(${Img3})`,backgroundSize:'cover',position:'relative'}}>
@@ -118,7 +118,7 @@ case "2":   return <Carousel key={2} className='Web-Development' responsive={res
                 </div>
                 <div className='Project-text' style={{fontFamily: 'Montserrat'}}>
                 Elevated Infinite Trails' travel business with a custom WordPress site featuring interactive tour packages and lightning-fast performance.
-                <Link to='/infinitetrails' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
+                <Link onClick={()=>{window.scroll(0,0)}}to='/infinitetrails' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
                 </div>
         </div>
         <div className='Project-Box' style={{width:'300px',height:'450px',margin:'10px 50px',backgroundImage:`url(${Img4})`,backgroundSize:'cover',position:'relative'}}>
@@ -127,7 +127,7 @@ case "2":   return <Carousel key={2} className='Web-Development' responsive={res
                 </div>
                 <div className='Project-text' style={{fontFamily: 'Montserrat'}}>
                 Crafted Himachal Darpan Live TV's website from scratch using WordPress and PHP, offering user-friendly access to Himachal Pradesh news.
-                <Link to='/himachaldarpan' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
+                <Link onClick={()=>{window.scroll(0,0)}}to='/himachaldarpan' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
                 </div>
         </div>
         <div className='Project-Box' style={{width:'300px',height:'450px',margin:'10px 50px',backgroundImage:`url(${Img5})`,backgroundSize:'cover',position:'relative'}}>
@@ -136,7 +136,7 @@ case "2":   return <Carousel key={2} className='Web-Development' responsive={res
                 </div>
                 <div className='Project-text' style={{fontFamily: 'Montserrat'}}>
                 Empowered PERGO, Shimla's non-profit, with a WordPress-PHP site showcasing their legal work and achievements effectively.
-                <Link to='/pergo' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
+                <Link onClick={()=>{window.scroll(0,0)}}to='/pergo' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
                 </div>
         </div>
 
@@ -146,7 +146,7 @@ case "2":   return <Carousel key={2} className='Web-Development' responsive={res
                 </div>
                 <div className='Project-text' style={{fontFamily: 'Montserrat'}}>
                 Elevated Creative Coaching Classes with a tailor-made WordPress-PHP site, spotlighting courses and student triumphs brilliantly.
-                <Link to='/creativecoaching' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
+                <Link onClick={()=>{window.scroll(0,0)}}to='/creativecoaching' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
                 </div>
         </div>
 
@@ -156,7 +156,7 @@ case "2":   return <Carousel key={2} className='Web-Development' responsive={res
                 </div>
                 <div className='Project-text' style={{fontFamily: 'Montserrat'}}>
                 Elevated Solitude Inn's bookings with an elegant website, optimized UI/UX, and lead-generating contact form.
-                <Link to='/solitudeinn' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
+                <Link onClick={()=>{window.scroll(0,0)}}to='/solitudeinn' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
                 </div>
         </div>
 
@@ -166,7 +166,7 @@ case "2":   return <Carousel key={2} className='Web-Development' responsive={res
                 </div>
                 <div className='Project-text' style={{fontFamily: 'Montserrat'}}>
                 Transformed Alpine Holiday Homes' online presence with an elegant website, boosting bookings per query through a sleek UI/UX and lead capture form.
-                <Link to='/alpine' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
+                <Link onClick={()=>{window.scroll(0,0)}}to='/alpine' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
                 </div>
         </div>
   </Carousel>;
@@ -179,7 +179,7 @@ case "3":   return <Carousel key={3} className='Social-Media-Management' respons
                 </div>
                 <div className='Project-text' style={{fontFamily: 'Montserrat'}}>
                 Elevated BJP candidates campaigns with captivating social media content and video editing for Himachal Pradesh elections 2021-2022.
-                <Link to='/bjp' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
+                <Link onClick={()=>{window.scroll(0,0)}}to='/bjp' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
                 </div>
         </div>
 
@@ -190,7 +190,7 @@ case "3":   return <Carousel key={3} className='Social-Media-Management' respons
                 </div>
                 <div className='Project-text' style={{fontFamily: 'Montserrat'}}>
                 Boosted HPPWD's social presence, reaching 2.8k organic Facebook followers, delivering vital info during the 2023 Himachal Floods.
-                <Link to='/pwd' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
+                <Link onClick={()=>{window.scroll(0,0)}}to='/pwd' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
                 </div>
         </div>
         <div className='Project-Box' style={{width:'300px',height:'450px',margin:'10px 50px',backgroundImage:`url(${Img19})`,backgroundSize:'cover',position:'relative'}}>
@@ -199,7 +199,7 @@ case "3":   return <Carousel key={3} className='Social-Media-Management' respons
                 </div>
                 <div className='Project-text' style={{fontFamily: 'Montserrat'}}>
                 Crafted engaging content for RPL Canada's cricket tournament, Realtors Premiere League, on Facebook and Instagram.
-                <Link to='/rpl' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
+                <Link onClick={()=>{window.scroll(0,0)}}to='/rpl' style={{textDecoration:'none'}}><div style={{margin:'20px',fontFamily:'BDSuper',color:'red',fontSize:'18px'}}>View</div></Link>
                 </div>
         </div>
   </Carousel>;
